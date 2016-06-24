ae3cbbab38aa3cd72a620cf28a0a9cbb4a1682a8
Thorsten Wißmann <edu@thorsten-wissmann.de>
2016-06-24 18:01:32 +0200

    Make pylemonbar a package

---
+++ pylemonbar/__init__.py
@@ -0,0 +1,14 @@
+"""A python panel using lemonbar"""
+
+import os.path
+
+__author__ = "Thorsten Wißmann"
+__copyright__ = "Copyright 2016 Thorsten Wißmann"
+__license__ = "GPL"
+__maintainer__ = __author__
+__email__ = "edu@thorsten-wissmann.de"
+__version_info__ = (0, 1, 0)
+__version__ = '.'.join(str(e) for e in __version_info__)
+__description__ = "A python panel using lemonbar"
+
+basedir = os.path.dirname(os.path.realpath(__file__))
